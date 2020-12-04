06f019686287bb4af120879f1771c832e1fe0d89
Víctor <victor@populate.tools>
2020-12-04 11:25:08 +0100

    Adding cpv category (just for the first cpv division) and contractor id

--- pipelines/datasets/licitaciones/query.sql
+++ pipelines/datasets/licitaciones/query.sql
@@ -1,6 +1,6 @@
 SELECT
   tenders.id,
-  title,
+  tenders.title,
   document_number,
   permalink,
   contract_statuses.text AS status,
@@ -14,12 +14,15 @@ SELECT
   number_of_batches,
   number_of_proposals,
   contractors.name AS contractor,
+  contractors.id AS contractor_id,
   contractors_types.text AS contractor_type,
   contract_value,
   initial_amount,
   initial_amount_no_taxes,
   array_to_string(tenders.cpvs, ',') AS cpvs,
-  process_types.text AS process_type
+  process_types.text AS process_type,
+  categories.id as category_id,
+  categories.title as category_title
 FROM
   tenders
   LEFT JOIN fiscal_entities contractors ON contractor_id = contractors.id
@@ -27,4 +30,6 @@ FROM
   LEFT JOIN contract_types ON contract_type = contract_types.id
   LEFT JOIN contract_statuses ON status = contract_statuses.id
   LEFT JOIN process_types ON tenders.process_type = process_types.id
+  LEFT JOIN cpv_categorizations ON cpv_categorizations.cpv_division = tenders.cpvs_divisions[1]
+  LEFT JOIN categories ON categories.id = cpv_categorizations.category_id
 WHERE contractors.custom_place_id = 8211
